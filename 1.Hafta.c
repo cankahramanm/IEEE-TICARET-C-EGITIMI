81f99e4893a521b9d509706b73ebb1fcad435c4d
cankahramanm <60780482+cankahramanm@users.noreply.github.com>
2020-12-12 19:12:30 +0300

    Update 1.Hafta.c

--- 1.Hafta.c
+++ 1.Hafta.c
@@ -20,6 +20,35 @@ int main()
 {
 int x;
 x=5;
+printf("X sayisi %d ye esittir.\n",x);
 return 0;
+}
+*/
+/*
+int main()
+{
+int a,b,toplam;
+a=2;
+b=9;
+toplam=a+b;
+printf("A ve B sayisinin toplami %d'e esittir.\n",toplam);
+//bunun başka bir yolu
+printf("------------------------------\n");
+printf("A ve B sayisinin toplami %d'e esittir.\n",a+b);
+return 0;
+}
+*/
+/*
+int main()
+{
+	int saat;
+	float ucret, toplam; //burda float ve charı kullanalım.
+	char harf;
+	ucret=50.595;
+  saat=12;
+  harf='A';
+	toplam = saat * ucret;
+	printf("%c kodlu calisanin, alacagi ucret: %f\n",harf,toplam);
+	return 0;
 }
 */
