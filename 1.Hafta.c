79200d1163fbf822be1ff8675e1dd2c4db9a575f
cankahramanm <60780482+cankahramanm@users.noreply.github.com>
2020-12-12 17:42:31 +0300

    Create 1.Hafta.c

---
+++ 1.Hafta.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/*
+int main()
+{
+  printf("Merhaba Arkadaslar");
+  printf("\nDersimize hepiniz hosgeldiniz.\n");
+  printf("-----------------------\n");
+  printf("Ben "
+		     "CAN "
+		     "EEE-3\n-----------------------\n");
+  printf("Merhaba Arkadaslar\nDersimize hepiniz hosgeldiniz.\n-----------------------\n");
+  printf("\t\tmerhaba\n-----------------------\n");
+  printf("Merhaba\n\tarkadaslar\n-----------------------\n");
+  printf("Can: \"Naber\" dedi.\n");
+  return 0;
+}
+*/
+/*
+int main()
+{
+int x;
+x=5;
+return 0;
+}
+*/
