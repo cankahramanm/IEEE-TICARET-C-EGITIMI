4c15dee37a046b4a3800c8effcc2dfba2e75d0da
cankahramanm <60780482+cankahramanm@users.noreply.github.com>
2020-12-19 21:52:49 +0300

    Some changes

---
+++ 2.Hafta Örnek Soru Cevapları.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/*
+int main(){
+
+    int a=5;
+    printf("2 ve 3 e tam bolunebilen sayilar 6 sayisina tam bolunebilir\n");
+    while (a<19){
+        if (a%3==0 && a%2==0){
+            printf("%d sayisi 6 ya tam bolunebilen sayi\n",a);
+        }
+        else {
+            printf("%d sayisi 6 ya tam bolunemez \n",a);
+
+        }
+        a++;
+    }
+    return 0;
+}
+*/
