4632e70b8b7f66aad3f13ab1ff5aac79f93e07ba
cankahramanm <cankahramanm@gmail.com>
2020-12-26 16:17:54 +0300

    Create 4.Hafta.c

---
+++ 4.Hafta.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+/*
+float fonk (float fonkparametre)
+{
+  return 10*fonkparametre;
+}
+
+int main()
+{
+  float fonkparametre;
+
+fonkparametre = 12.75;
+
+  printf("%f\n", fonk (fonkparametre));
+
+  return 0;
+}
+*/
+/*
+void fonk (int id)
+{
+  id = id + 27;
+  printf("Fonksiyon icinde id degişken degeri: %d\n", id);
+}
+int main ()
+{
+  int id;
+  id = 34;
+
+  fonk (id);
+
+  printf("main() fonksiyonu icinde id degisken degeri: %d\n", id);
+
+  return 0;
+}
+*/
+/*
+int topla(int a, int b)
+ {
+     return a+b;
+ }
+
+ int carp(int a, int b)
+ {
+     return a*b;
+ }
+
+
+ int toplamlarinKaresi(int a, int b)
+ {
+     int ikiSayininToplami = topla(a,b);
+     return carp(ikiSayininToplami,ikiSayininToplami );
+ }
+
+ int main()
+ {
+    int birinciSayi, ikinciSayi;
+    printf("Lutfen iki sayi giriniz!");
+    printf("\nilk sayi:");
+    scanf("%d", &birinciSayi);
+    printf("ikincisi : ");
+    scanf("%d", &ikinciSayi);
+
+    printf("iki sayinin toplami: %d \n", topla(birinciSayi,ikinciSayi));
+    printf("iki sayinin carpimi: %d \n", carp(birinciSayi,ikinciSayi));
+    printf("iki sayinin toplamlarinin karesi: %d \n", toplamlarinKaresi(birinciSayi,ikinciSayi));
+    return 0;
+ }
+*/
